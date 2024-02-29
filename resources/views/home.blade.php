6d7aaf6530195c09ca8a40ab087db2ad9539122a
CLINTON1233 <clintonalfaro664@example.com>
2024-02-29 23:15:47 +0700

    Menyiapkan kerangka MVC untuk PBL

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    Selamat datang dihome {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+    
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('contact', [HomeController::class, 'contact']);
+
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
+// Route::get('/user/{id}', function ($id) {
+//     return 'User dengan ID ' . $id;
+// });
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/dashboard', function () {
+//         return 'Admin Dashboard';
+//     });
+
+//     Route::get('/users', function () {
+//         return 'Admin Users';
+//     });
+// });
+
+Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+    return view('list_barang', compact('id', 'nama'));
 });
+
+// Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+// Login Routes
+Route::get('/login_webpajak', [LoginController::class, 'index']);
