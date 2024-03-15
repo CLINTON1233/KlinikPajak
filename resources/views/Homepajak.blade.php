47bb5087a9ba28f75f22c3ac1f8bc8043e55744b
CLINTON1233 <clintonalfaro664@example.com>
2024-03-15 21:18:10 +0700

    update

--- resources/views/Homepajak.blade.php
+++ resources/views/Homepajak.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sudut Pajak</title>
     <!-- Tambahkan link ke Tailwind CSS -->
-    <link rel="stylesheet" href="{{ asset('PBL-25/style/tailwindcss3.4.1.js') }}">
+    <script src="{{ asset('style/tailwindcss3.4.1.js') }}">
+    </script>
     <style>
         /* CSS untuk mengatur tata letak gambar tidak diperlukan lagi */
 
@@ -31,19 +32,19 @@
 <body class="bg-gray-100">
     <!-- Tambahkan kelas background warna abu-abu pada body menggunakan Tailwind CSS -->
     <center>
-        <h1 class="text-3xl font-bold my-4">Selamat Datang di Website Sudut Pajak</h1>
+        <h1 class="text-2xl font-bold my-4">Selamat Datang di Website Sudut Pajak</h1>
         <!-- Menggunakan kelas Tailwind untuk mengatur ukuran teks dan margin atas/bawah -->
     </center>
-    <p class="mx-4">
+    <p class="mx-1">
         <!-- Menggunakan kelas Tailwind untuk mengatur margin horizontal pada paragraf -->
         Kami "sudut pajak" merupakan layanan pajak berbasis website.
         Saat ini sudut pajak memberikan layanan perpajakan online secara gratis baik konsultasi maupun secara teori dengan tujuan untuk memberikan solusi permasalahan perpajakan oleh wajib pajak yang terdaftar dan yang belum terdaftar.
         Fokus utama sudut pajak saat ini adalah untuk mendampingi pelaku UMKM dalam memahami peraturan perpajakan.
     </p>
 
-    <div class="flex flex-col items-center mt-2">
+    <div class="flex flex-col items-center mt-0">
         <!-- Menggunakan kelas Flexbox dan align-center untuk mengatur tata letak gambar -->
-        <img src="{{ asset('img/BeritaPajak.jpg') }}" alt="BeritaPajak" class="max-w-full mb-4">
+        <img src="{{ asset('img/BeritaPajak.jpg') }}" alt="BeritaPajak" class="max-w-full mb-1">
         <!-- Menggunakan kelas Tailwind untuk mengatur lebar maksimum gambar dan margin bawah -->
         <img src="{{ asset('img/GambarPajak.png') }}" alt="GambarPajak" class="max-w-full">
         <!-- Menggunakan kelas Tailwind untuk mengatur lebar maksimum gambar -->
